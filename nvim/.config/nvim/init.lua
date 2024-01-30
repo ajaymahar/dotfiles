08b4a68712b9b1ed86f29f72b2986793938792e8
Ajay Kumar Mahar <ajay.mhar@gmail.com>
2024-01-30 13:58:26 +0530

    adding neovim config to dotfiles
    added nvim config to dotfile repo
    added db_ui and lazy lock file to gitignore

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,17 @@
+require('keymaps')
+require('plugins.lazy')
+require('plugins.misc')
+require('plugins.lualine')
+require('options')
+require('misc')
+require('plugins.dap')
+require('plugins.gitsigns')
+require('plugins.tele')
+require('plugins.treesitter')
+require('plugins.lsp')
+require('plugins.trouble')
+require('plugins.comment')
+require('plugins.barbar')
+require('plugins.obsidian')
+-- vim: ts=8 sts=2 sw=2 et
+-- this is a comment

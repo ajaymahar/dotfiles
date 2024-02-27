7efdce1c68329f0dbc0c42961de1f293bb0cec97
Ajay Kumar Mahar <ajay.mhar@gmail.com>
2024-02-27 15:20:56 +0530

    [fish] update fzf theme for default option

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -49,7 +49,9 @@ set -Ux FZF_DEFAULT_OPTS "\
 --pointer=' ' \
 --marker=' ' \
 --ansi \
---color='16,bg+:-1,gutter:-1,prompt:5,pointer:5,marker:6,border:4,label:4,header:italic'"
+--color=bg+:#313244,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,prompt:#cba6f7,hl+:#f38ba8"
 
 set -Ux FZF_TMUX_OPTS "-p 55%,60%"
 set -Ux FZF_CTRL_R_OPTS "--border-label=' history ' --prompt='  '"

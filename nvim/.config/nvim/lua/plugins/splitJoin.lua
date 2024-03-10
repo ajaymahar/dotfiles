e59c144577739f28386b921d25f27ba0d3ce8a56
Ajay Kumar Mahar <ajay.mhar@gmail.com>
2024-03-10 21:17:08 +0530

    [nvim] add comment for split join keybindings
    gS to split
    gJ to join

--- nvim/.config/nvim/lua/plugins/splitJoin.lua
+++ nvim/.config/nvim/lua/plugins/splitJoin.lua
@@ -2,6 +2,9 @@ return {
   {
     'AndrewRadev/splitjoin.vim',
     config = function()
+      -- Keymapping for split and join
+      -- gS to split
+      -- gJ to join
       vim.api.nvim_set_var('splitjoin_quiet', 1)
       vim.api.nvim_set_var('splitjoin_trailing_comma', 1)
     end

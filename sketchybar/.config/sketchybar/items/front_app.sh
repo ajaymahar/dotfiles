58f1474304c6d7891b7912d069ba8862101d8471
Ajay Kumar Mahar <ajay.mhar@gmail.com>
2025-02-06 23:34:16 +0530

    [sketchybar] cleanup

--- sketchybar/.config/sketchybar/items/front_app.sh
+++ sketchybar/.config/sketchybar/items/front_app.sh
@@ -3,6 +3,12 @@
 sketchybar --add item front_app left \
            --set front_app       background.color=$ACCENT_COLOR \
                                  icon.color=$BAR_COLOR \
+                                 label.padding_right=20                     \
+                                 label.y_offset=-1                          \
+                                 icon.padding_left=10 \
+                                 icon.padding_right=10 \
+                                 padding_left=2\
+                                 padding_right=2\
                                  icon.font="sketchybar-app-font:Regular:16.0" \
                                  label.color=$BAR_COLOR \
                                  script="$PLUGIN_DIR/front_app.sh"            \
